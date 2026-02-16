807447709bcf50a8bbf67a2412984d0e8e7fdc31
VladMalaxa <imvladmalaxa@gmail.com>
2026-02-16 11:13:05 +0100

    assignment 2
    will do some formatting to be in accordance with the lab manual. Besides that, it is finished.

---
+++ G2Assignment1_2/README.txt
@@ -0,0 +1,23 @@
+This folder contains the files relevant to Assignment 1.2 of Software Development for Robotics and was created by group 'PPD 2',
+This group is composed by:
+
+Vlad Malaxa	-	s2726254
+Ricardo Díaz	-	s3681548
+
+In order to compile, the BMI.cpp and testing.cpp need to be compiled separately. 
+Typing the following lines in the command line will successfully compile the files and give the desired output: 
+
+for the BMI code:
+g++ BMI.cpp -o BMI
+
+for the testing code:
+g++ testing.cpp -o test
+
+
+After compiling is successful typing the following will run the program:
+
+for the BMI code:
+.\BMI.exe
+
+for the testing code:
+.\test.exe
---
+++ assignment2.cpp
@@ -0,0 +1,125 @@
+#include <array>
+#include <iostream>
+#include <utility>   // std::pair
+#include <cstdlib>   // std::exit
+
+constexpr int SIZE = 12;
+using Maze = std::array<std::array<char, SIZE>, SIZE>;
+
+enum class Direction
+{
+    Down,
+    Right,
+    Up,
+    Left
+};
+
+void printMaze(const Maze& maze)
+{
+    for (const auto& row : maze)
+    {
+        for (char c : row)
+            std::cout << c << ' ';
+        std::cout << '\n';
+    }
+    std::cout << "------------------------\n";
+}
+
+std::pair<int, int> findStart(const Maze& maze)
+{
+    for (int r = 0; r < SIZE; ++r)
+        for (int c = 0; c < SIZE; ++c)
+            if (maze[r][c] == 'x')
+                return {r, c};
+
+    return {-1, -1}; // not found
+}
+
+bool isInside(int r, int c)
+{
+    return r >= 0 && r < SIZE && c >= 0 && c < SIZE;
+}
+
+// Exit rule: any boundary cell that is not the starting cell.
+bool isExit(int r, int c, int startR, int startC)
+{
+    bool onBoundary = (r == 0 || r == SIZE - 1 || c == 0 || c == SIZE - 1);
+    return onBoundary && !(r == startR && c == startC);
+}
+
+void move(Direction dir, int& r, int& c)
+{
+    switch (dir)
+    {
+        case Direction::Down:  ++r; break;
+        case Direction::Right: ++c; break;
+        case Direction::Up:    --r; break;
+        case Direction::Left:  --c; break;
+    }
+}
+
+// Recursive traversal: moves only onto '.' squares, marks visited with 'x',
+// prints after each move, and exits when an exit is reached.
+void traverseMaze(Maze& maze, int r, int c, int startR, int startC)
+{
+    if (isExit(r, c, startR, startC))
+    {
+        printMaze(maze);
+        std::cout << "Maze is solved!\n";
+        std::exit(0);
+    }
+
+    Direction directions[4] = {
+        Direction::Down,
+        Direction::Right,
+        Direction::Up,
+        Direction::Left
+    };
+
+    for (Direction dir : directions)
+    {
+        int nr = r;
+        int nc = c;
+
+        move(dir, nr, nc);
+
+        if (isInside(nr, nc) && maze[nr][nc] == '.')
+        {
+            maze[nr][nc] = 'x';   // mark visited
+            printMaze(maze);      // display after each move
+            traverseMaze(maze, nr, nc, startR, startC);
+        }
+    }
+}
+
+int main()
+{
+    Maze maze = {{
+        {{'#','#','#','#','#','#','#','#','#','#','#','#'}},
+        {{'#','.','.','.','#','.','.','.','.','.','.','#'}},
+        {{'.','.','#','.','#','.','#','#','#','#','.','#'}},
+        {{'#','#','#','.','#','.','.','.','.','#','.','#'}},
+        {{'#','.','.','.','.','#','#','#','.','#','.','x'}},
+        {{'#','#','#','#','.','#','.','#','.','#','.','#'}},
+        {{'#','.','.','#','.','#','.','#','.','#','.','#'}},
+        {{'#','.','.','#','.','#','.','#','.','#','.','#'}},
+        {{'#','.','.','.','.','.','.','.','.','#','.','#'}},
+        {{'#','#','#','#','#','#','.','#','#','#','.','#'}},
+        {{'#','.','.','.','.','.','.','#','.','.','.','#'}},
+        {{'#','#','#','#','#','#','#','#','#','#','#','#'}}
+    }};
+
+    auto [startR, startC] = findStart(maze);
+    if (startR == -1)
+    {
+        std::cout << "Error: start position 'x' not found.\n";
+        return 1;
+    }
+
+    printMaze(maze);
+    traverseMaze(maze, startR, startC, startR, startC);
+
+    // If no exit exists, recursion will eventually return here.
+    std::cout << "No exit found.\n";
+    return 0;
+}
